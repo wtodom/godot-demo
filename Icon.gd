18a76d1a601032bf85ae54548a36dfb641e9a566
Weston Odom <weston.odom@gmail.com>
2023-12-18 17:43:30 -0500

    starting godot tutorial

---
+++ Icon.gd
@@ -0,0 +1,28 @@
+extends Sprite2D
+
+
+var speed = 400
+var angular_speed = PI
+
+func _process(delta):	
+	if Input.is_action_pressed("quit"):
+		get_tree().quit()
+
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
+
+	rotation += angular_speed * direction * delta
+	
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * speed
+		
+	position += velocity * delta
+		
+#func _unhandled_input(event):
+	#if event is InputEventKey:
+		#if event.pressed and event.keycode == KEY_F6:
+			#get_tree().quit()
